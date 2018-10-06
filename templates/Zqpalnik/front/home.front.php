2474e5f5172db2ae2e900079bf5abfd52e290040
dkirilov <dian.kirilov@gmail.com>
2018-10-06 03:09:09 +0300

    modified:   templates/Zqpalnik/front/css/customiz.css 	modified:   templates/Zqpalnik/front/home.front.php

---
+++ templates/Zqpalnik/front/home.front.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <div class="col-xs">
+       <div class="box">
+	        <section id="home_welcome_section">
+			     <h2>Welcome to our great <?= SITE_NAME ?>, dear visitor! We are happy to see you here!</h2>
+				 <div>
+				      Here you can watch plenty of videos, movies and tv shows in variety of genres and topics.  
+					  In order you can watch some of the videos, you will need to make  an account by passing through our <a href="#">Sign up</a> process.
+					  After you pass through the <a href="#">Sign up</a> process, then you can do also and the following things: 
+                      <ul>					      
+						  <li>Upload your own videos,</li>
+						   <li>Create your own channels,</li>
+						   <li>Create your own playlists,</li>
+						   <li>Save your favorite videos and playlists to "My favorites" list,</li>
+						   <li>Follow channels, created by other users,</li>
+						   <li>You can have fans and friendships,</li>
+						   <li>You can communicate with the others through our text chat,</li>
+						   <li> ... and some others.</li>
+					 </ul>
+					 
+					 We wish you great experience at our site! We take care to be so. If you find something wrong or have questions , please feel free to <a href="#">contact us</a>.
+					 <br>
+					 <button type="button" class="btn-small-blue">O.K. I understand. Don't show me this message again, please!</button>
+				 </div>
+			</section>
+	   </div>
+    </div>    
+</div>
